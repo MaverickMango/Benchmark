9a379a7c0ec116759c722685be1a5e826f346c66
MaverickMango <1397959741@qq.com>
2023-10-19 05:42:34 -0700

    add mutators and transformation part for generation task

--- src/main/java/root/generation/helper/MutatorHelper.java
+++ src/main/java/root/generation/helper/MutatorHelper.java
@@ -1,106 +1,46 @@
 package root.generation.helper;
 
-import org.apache.commons.io.FileUtils;
+import com.github.javaparser.ast.expr.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import root.generation.parser.ASTJDTParser;
-import root.generation.parser.ASTJavaParser;
-import root.generation.parser.AbstractASTParser;
-import root.util.ConfigurationProperties;
+import root.generation.transformation.mutator.*;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.*;
 
 public class MutatorHelper {
+
     private static final Logger logger = LoggerFactory.getLogger(MutatorHelper.class);
-    protected String complianceLevel;
-    protected String srcJavaDir;
-    protected String srcTestDir;
-    protected String binJavaDir;
-    protected String binTestDir;
-    protected Set<String> dependencies;
-    protected String javaClassesInfoPath;
-    protected String testClassesInfoPath;
-    protected Set<String> binJavaClasses;
-    protected Set<String> binExecuteTestClasses;
-    AbstractASTParser parser;
-    protected Map<String, Object> ASTs;//CompilationUnit
-    protected List<String> compilerOptions;
 
-    public MutatorHelper() {
-        complianceLevel = ConfigurationProperties.getProperty("complianceLevel");
-        String location = ConfigurationProperties.getProperty("location");
-        binJavaDir = location + File.separator + ConfigurationProperties.getProperty("binJavaDir");
-        binTestDir = location + File.separator + ConfigurationProperties.getProperty("binTestDir");
-        srcJavaDir = location + File.separator + ConfigurationProperties.getProperty("srcJavaDir");
-        srcTestDir = location + File.separator + ConfigurationProperties.getProperty("srcTestDir");
-        String tmp = ConfigurationProperties.getProperty("dependencies");
-        if (tmp != null) {
-            dependencies = new HashSet<>();
-            dependencies.addAll(Arrays.asList(tmp.split(File.pathSeparator)));
-        }
+    public static Map<Class<? extends Expression>, AbstractInputMutator> MUTATORS;
+    public static Map<String, List<Class<? extends Expression>>> INPUTS_BY_TYPE;
 
-        tmp = ConfigurationProperties.getProperty("binExecuteTestClasses");
-        if (tmp != null) {
-            binExecuteTestClasses = new HashSet<>();
-            binExecuteTestClasses.addAll(Arrays.asList(tmp.split(File.pathSeparator)));
-        }
-        javaClassesInfoPath = ConfigurationProperties.getProperty("javaClassesInfoPath");
-        testClassesInfoPath = ConfigurationProperties.getProperty("testClassesInfoPath");
+    public static void initialize() {
+        //todo: collect all values of different types in test file?
+        MUTATORS = new HashMap<>();
+        MUTATORS.put(BooleanLiteralExpr.class, new BooleanMutator());
+        MUTATORS.put(DoubleLiteralExpr.class, new DoubleMutator());
+        MUTATORS.put(IntegerLiteralExpr.class, new IntegerMutator());
+        MUTATORS.put(LongLiteralExpr.class, new LongMutator());
+        MUTATORS.put(StringLiteralExpr.class, new StringMutator());
+
+        INPUTS_BY_TYPE = new HashMap<>();
+        INPUTS_BY_TYPE.put("boolean", Collections.singletonList(BooleanLiteralExpr.class));
+        INPUTS_BY_TYPE.put("java.util.Boolean", Collections.singletonList(BooleanLiteralExpr.class));
+        INPUTS_BY_TYPE.put("int", Collections.singletonList(IntegerLiteralExpr.class));
+        INPUTS_BY_TYPE.put("java.lang.Integer", Collections.singletonList(IntegerLiteralExpr.class));
+        INPUTS_BY_TYPE.put("double", Collections.singletonList(DoubleLiteralExpr.class));
+        INPUTS_BY_TYPE.put("java.lang.Double", Collections.singletonList(DoubleLiteralExpr.class));
+        INPUTS_BY_TYPE.put("long", Collections.singletonList(LongLiteralExpr.class));
+        INPUTS_BY_TYPE.put("java.lang.Long", Collections.singletonList(DoubleLiteralExpr.class));
+        INPUTS_BY_TYPE.put("java.lang.String", Collections.singletonList(StringLiteralExpr.class));
+        List<Class<? extends Expression>> list = Arrays.asList(
+                BooleanLiteralExpr.class, IntegerLiteralExpr.class,
+                DoubleLiteralExpr.class, StringLiteralExpr.class);
+        INPUTS_BY_TYPE.put("Object", list);
+        INPUTS_BY_TYPE.put("java.lang.Object", list);
     }
 
-    public void initialize(boolean sourceOnly, boolean testOnly) throws IOException, ClassNotFoundException {
-        if (!sourceOnly) {
-            invokeClassFinder();//?
-            invokeCompilerOptionInitializer(complianceLevel);
-        }
-        invokeSourceASTParser(testOnly);
-        invokeExtractors();
-    }
-
-    private void invokeClassFinder() throws ClassNotFoundException, IOException {
-        ClassFinder finder = new ClassFinder(binJavaDir, binTestDir, dependencies);
-        binJavaClasses = finder.findBinJavaClasses();
-
-        if (binExecuteTestClasses == null)
-            binExecuteTestClasses = finder.findBinExecuteTestClasses();
-
-        if (javaClassesInfoPath != null)
-            FileUtils.writeLines(new File(javaClassesInfoPath), binJavaClasses);
-        if (testClassesInfoPath != null)
-            FileUtils.writeLines(new File(testClassesInfoPath), binExecuteTestClasses);
-    }
-
-    private void invokeSourceASTParser(boolean testOnly) throws IOException {
-        logger.info("Invoking source code ast parse...");
-
-        parser = new ASTJavaParser(srcJavaDir, srcTestDir, dependencies);//resolver是白给的么
-//        parser = new ASTJDTParser(srcJavaDir, srcTestDir, dependencies, new HashMap<>());//很好这两个都没有类型解析啊啊啊
-        if (!testOnly)
-            parser.parseASTs(srcJavaDir);
-        parser.parseASTs(srcTestDir);
-        ASTs = parser.getASTs();
-
-        logger.info("AST parsing is finished!");
-    }
-
-    private void invokeCompilerOptionInitializer(String complianceLevel) {
-        compilerOptions = new ArrayList<>();
-        compilerOptions.add("-nowarn");
-        compilerOptions.add("-source");
-        compilerOptions.add(complianceLevel);
-        compilerOptions.add("-cp");
-        StringBuilder cpStr = new StringBuilder(binJavaDir);
-        if (dependencies != null) {
-            for (String path: dependencies) {
-                cpStr.append(File.pathSeparator).append(path);
-            }
-        }
-        compilerOptions.add(cpStr.toString());
-    }
-
-    private void invokeExtractors() {
-
+    public static boolean isKnownClass(String className) {
+        return INPUTS_BY_TYPE.containsKey(className);
     }
 }
---
+++ src/main/java/root/generation/helper/Preparation.java
@@ -0,0 +1,112 @@
+package root.generation.helper;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import root.generation.transformation.extractor.InputExtractor;
+import root.generation.parser.ASTJavaParser;
+import root.generation.parser.AbstractASTParser;
+import root.util.ConfigurationProperties;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class Preparation {
+    private static final Logger logger = LoggerFactory.getLogger(Preparation.class);
+    public String complianceLevel;
+    public String srcJavaDir;
+    public String srcTestDir;
+    public String binJavaDir;
+    public String binTestDir;
+    public Set<String> dependencies;
+    public String javaClassesInfoPath;
+    public String testClassesInfoPath;
+    public Set<String> binJavaClasses;
+    public Set<String> binExecuteTestClasses;
+    public AbstractASTParser parser;
+    public Map<String, Object> ASTs;//CompilationUnit
+    public List<String> compilerOptions;
+    public InputExtractor inputExtractor;
+
+    public Preparation() {
+        complianceLevel = ConfigurationProperties.getProperty("complianceLevel");
+        String location = ConfigurationProperties.getProperty("location");
+        Path path = Paths.get(location).toAbsolutePath();
+        binJavaDir = path.resolve(ConfigurationProperties.getProperty("binJavaDir")).normalize().toString();
+        binTestDir = path.resolve(ConfigurationProperties.getProperty("binTestDir")).normalize().toString();
+        srcJavaDir = path.resolve(ConfigurationProperties.getProperty("srcJavaDir")).normalize().toString();
+        srcTestDir = path.resolve(ConfigurationProperties.getProperty("srcTestDir")).normalize().toString();
+        String tmp = ConfigurationProperties.getProperty("dependencies");
+        if (tmp != null) {
+            dependencies = new HashSet<>();
+            dependencies.addAll(Arrays.asList(tmp.split(File.pathSeparator)));
+        }
+
+        tmp = ConfigurationProperties.getProperty("binExecuteTestClasses");
+        if (tmp != null) {
+            binExecuteTestClasses = new HashSet<>();
+            binExecuteTestClasses.addAll(Arrays.asList(tmp.split(File.pathSeparator)));
+        }
+        javaClassesInfoPath = ConfigurationProperties.getProperty("javaClassesInfoPath");
+        testClassesInfoPath = ConfigurationProperties.getProperty("testClassesInfoPath");
+    }
+
+    public void initialize(boolean sourceOnly, boolean testOnly) throws IOException, ClassNotFoundException {
+        if (!sourceOnly) {
+            invokeClassFinder();//?
+            invokeCompilerOptionInitializer(complianceLevel);
+        }
+        invokeSourceASTParser(testOnly);
+        invokeTransformation();
+    }
+
+    private void invokeClassFinder() throws ClassNotFoundException, IOException {
+        ClassFinder finder = new ClassFinder(binJavaDir, binTestDir, dependencies);
+        binJavaClasses = finder.findBinJavaClasses();
+
+        if (binExecuteTestClasses == null)
+            binExecuteTestClasses = finder.findBinExecuteTestClasses();
+
+        if (javaClassesInfoPath != null)
+            FileUtils.writeLines(new File(javaClassesInfoPath), binJavaClasses);
+        if (testClassesInfoPath != null)
+            FileUtils.writeLines(new File(testClassesInfoPath), binExecuteTestClasses);
+    }
+
+    private void invokeSourceASTParser(boolean testOnly) throws IOException {
+        logger.info("Invoking source code ast parse...");
+
+        parser = new ASTJavaParser(srcJavaDir, srcTestDir, dependencies);//resolver是白给的么
+//        parser = new ASTJDTParser(srcJavaDir, srcTestDir, dependencies, new HashMap<>());//很好这两个都没有类型解析啊啊啊
+        if (!testOnly)
+            parser.parseASTs(srcJavaDir);
+        parser.parseASTs(srcTestDir);
+        ASTs = parser.getASTs();
+
+        logger.info("AST parsing is finished!");
+    }
+
+    private void invokeCompilerOptionInitializer(String complianceLevel) {
+        compilerOptions = new ArrayList<>();
+        compilerOptions.add("-nowarn");
+        compilerOptions.add("-source");
+        compilerOptions.add(complianceLevel);
+        compilerOptions.add("-cp");
+        StringBuilder cpStr = new StringBuilder(binJavaDir);
+        if (dependencies != null) {
+            for (String path: dependencies) {
+                cpStr.append(File.pathSeparator).append(path);
+            }
+        }
+        compilerOptions.add(cpStr.toString());
+    }
+
+    private void invokeTransformation() {
+        //todo: test extractor/ assert / arguments
+        inputExtractor = new InputExtractor(parser);
+        MutatorHelper.initialize();
+    }
+}
